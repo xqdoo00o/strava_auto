410a34dd9c2ad3129d9fc4f2b6f15754ebaaf405
David xu <xqdoo00o@gmail.com>
2026-04-30 05:02:24 +0800

    fix linux app icon

--- lib/native_logic.dart
+++ lib/native_logic.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'package:flutter/services.dart';
 import 'package:path/path.dart' as p;
 import 'package:win32_registry/win32_registry.dart';
 import 'log_manager.dart';
@@ -32,6 +33,8 @@ Future<void> registerDesktopCustomProtocol() async {
 Name=Strava Auto
 Comment=Automate your Strava data
 Exec=$appPath %u
+Icon=com.upstrava
+StartupWMClass=com.upstrava
 Type=Application
 Terminal=false
 MimeType=x-scheme-handler/$scheme;
@@ -39,17 +42,27 @@ Categories=Utility;
 """;
     String homeDir = Platform.environment['HOME'] ?? "";
     if (homeDir.isEmpty) return;
-    File desktopFile = File(
-      p.join(homeDir, '.local/share/applications', 'strava_auto.desktop'),
+    File iconFile = File(
+      p.join(homeDir, '.local/share/icons', 'com.upstrava.png'),
     );
+    File desktopFile = File(
+      p.join(homeDir, '.local/share/applications', 'com.upstrava.desktop'),
+    );
+
     try {
+      if (!await iconFile.exists()) {
+        await iconFile.parent.create(recursive: true);
+        ByteData data = await rootBundle.load('assets/icon.png');
+        List<int> bytes = data.buffer.asUint8List(
+          data.offsetInBytes,
+          data.lengthInBytes,
+        );
+        await iconFile.writeAsBytes(bytes);
+      }
       if (!await desktopFile.parent.exists()) {
         await desktopFile.parent.create(recursive: true);
       }
       await desktopFile.writeAsString(desktopContent);
-      await Process.run('update-desktop-database', [
-        p.dirname(desktopFile.path),
-      ]);
     } catch (e) {
       LogManager().addLog('Protocol Error: $e', isError: true);
     }
