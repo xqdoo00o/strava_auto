556a1c11b39ea91c19dc5ad65152bdd7eee55599
David xu <xqdoo00o@gmail.com>
2026-06-12 02:57:20 +0800

    fix: chrome extension multi open

--- web/background.js
+++ web/background.js
@@ -1,5 +1,14 @@
-chrome.action.onClicked.addListener((tab) => {
-  chrome.tabs.create({
-    url: chrome.runtime.getURL("index.html"),
-  });
+chrome.action.onClicked.addListener(() => {
+    const appUrl = chrome.runtime.getURL("index.html");
+    chrome.tabs.query({}, (tabs) => {
+        const appTab = tabs.find((tab) => tab.url && tab.url.startsWith(appUrl));
+        if (appTab?.id) {
+            chrome.tabs.update(appTab.id, { active: true });
+            if (appTab.windowId) {
+                chrome.windows.update(appTab.windowId, { focused: true });
+            }
+            return;
+        }
+        chrome.tabs.create({ url: appUrl });
+    });
 });
